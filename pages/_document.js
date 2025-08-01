02814abe1e9102dd9079e94101bd3cc157db68ae
jimspi <jpsthesecond@gmail.com>
2025-08-01 16:07:37 -0600

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
         <link rel="apple-touch-icon" href="/favicon.png" />
+        <link rel="shortcut icon" href="/favicon.png" />
         <meta name="theme-color" content="#2563eb" />
         <meta name="description" content="AI-powered editorial workflow for journalism teams" />
       </Head>
